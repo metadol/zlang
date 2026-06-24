b150f9f0d405de466f5cc161df39ac9e4955d22b
Kevin Abraham <kevingitabr@gmail.com>
2026-06-24 20:32:10 +0530

    fix(header): adjust margin for better layout consistency

--- src/app/(main)/learn/_components/header.tsx
+++ src/app/(main)/learn/_components/header.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Header = ({ title }: Props) => {
   return (
-    <div className="bg-white sticky top-0 pb-3 lg:pt-7 flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
+    <div className="bg-white sticky top-0 pb-3 lg:pt-7 lg:-mt-7 flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
       <Link href="/courses">
         <Button variant={"ghost"} size={"sm"}>
           <ArrowLeft size={20} strokeWidth={2} className="text-neutral-400" />
--- src/components/layout/feed-wrapper.tsx
+++ src/components/layout/feed-wrapper.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 export const FeedWrapper = ({ children }: Props) => {
   return (
-    <div className="flex-1 bg-white relative top-0">
+    <div className="flex-1 bg-white relative top-0 pb-10">
       <div className="">
         {children}
       </div>

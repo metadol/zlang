c8d44077aa7d42c5e3c437a35c7dca73c7458744
Kevin Abraham <kevingitabr@gmail.com>
2026-06-25 08:40:46 +0530

    feat(lesson-button): implement zigzag lesson button logic

--- src/app/(main)/learn/_components/lesson-button.tsx
+++ src/app/(main)/learn/_components/lesson-button.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Check, Crown, Star } from "lucide-react";
+
 type Props = {
   id: number;
   index: number;
@@ -17,5 +19,29 @@ export const LessonButton = ({
   current,
   percenrage,
 }: Props) => {
-  return <div>Lesson Button{id}</div>;
+  const cycleLength = 8;
+  const cycleIndex = index % cycleLength;
+
+  let indentationLevel;
+  if (cycleIndex <= 2) {
+    indentationLevel = cycleIndex;
+  } else if (cycleIndex <= 4) {
+    indentationLevel = 4 - cycleIndex;
+  } else if (cycleIndex <= 6) {
+    indentationLevel = 4 - cycleIndex;
+  } else {
+    indentationLevel = cycleIndex - 8;
+  }
+
+  const rightPosition = indentationLevel * 40;
+
+  const isFirst = index === 0;
+  const isLast = index === totalCount;
+  const isCompleted = !current && !locked;
+
+  const Icon = isCompleted ? Check : isLast ? Crown : Star
+  const href = isCompleted ? `/lesson/${id}` : `/lesson`
+
+
+  return <div className="flex">Lesson Button{id}<Star/> <Check/><Crown/></div>;
 };
